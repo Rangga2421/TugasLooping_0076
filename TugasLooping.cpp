d5f26d876200ef22b586ddd7a7b92bcf2fbc2b64
Rangga2421 <ranggaardisaputra123@gmail.com>
2026-03-31 21:10:12 +0700

    menambahkan perulangan while pada isFibonacci

--- TugasLooping.cpp
+++ TugasLooping.cpp
@@ -1,3 +1,13 @@
 bool isFibonacci(int n) {
     int a = 0, b = 1, c = 0;
+
+    while (c <= n) {
+        if (c == n) {
+            return true;
+        }
+        a = b;
+        b = c;
+        c = a + b;
+    }
+    return false;
 }
