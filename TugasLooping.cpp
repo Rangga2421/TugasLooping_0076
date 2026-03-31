16fed9cd778252f61d5b376fbd76a8d990f0023d
Rangga2421 <ranggaardisaputra123@gmail.com>
2026-03-31 21:10:54 +0700

    menambahkan prosedur input angka

--- TugasLooping.cpp
+++ TugasLooping.cpp
@@ -1,13 +1,4 @@
-bool isFibonacci(int n) {
-    int a = 0, b = 1, c = 0;
-
-    while (c <= n) {
-        if (c == n) {
-            return true;
-        }
-        a = b;
-        b = c;
-        c = a + b;
-    }
-    return false;
+void inputAngka() {
+    cout << "Masukkan angka: ";
+    cin >> angka;
 }
