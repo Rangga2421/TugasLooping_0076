ab9d7e7a540263e074898a5d65ce547e06a16187
Rangga2421 <ranggaardisaputra123@gmail.com>
2026-03-31 21:14:57 +0700

    menambahkan input pilihan pada main

--- TugasLooping.cpp
+++ TugasLooping.cpp
@@ -1,7 +1,6 @@
-void menu() {
-    cout << "\n=== MENU ===\n";
-    cout << "1. Cek Bilangan Prima\n";
-    cout << "2. Cek Bilangan Fibonacci\n";
-    cout << "0. Keluar\n";
-    cout << "Pilih menu: ";
+int main() {
+    menu();
+    cin >> pilihan;
+
+    return 0;
 }
